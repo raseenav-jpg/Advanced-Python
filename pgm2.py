7b6f9f0359dfea2700effe026cb5cd71e6a5b579
raseenav-jpg <raseenav@siasindia.org>
2026-05-15 11:48:03 +0530

    Add files via upload

---
+++ pgm2.py
@@ -0,0 +1,15 @@
+name=input("enter your name\n")
+age=input("enter your age\n")
+school=input("enter your school\n")
+place=input("enter your place\n")
+dob=input("enter your dob\n")
+print("hi my nameis",name,"\nIam" ,age," year old.\n iam studying in" ,school,"school \n iam coming from",place,"\n  my dob is",dob,)
+
+
+
+
+
+
+
+
+
