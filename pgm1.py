11072afa7a1f04bd8d226ca8a330ae1fa6d50480
raseenav-jpg <raseenav@siasindia.org>
2026-05-15 10:59:30 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,8 @@
+name="liyan"
+age=15
+school="mes"
+place="calicut"
+program="python"
+fav="hobies"
+print("hi my nameis",name,"\nIam" ,age," year old.\n iam studying in" ,school,"school \n iam coming from",place,"\n i like coding in",program,"\n my ",fav,"are :playing football" )
+print(f"hi my name is{name}\n iam{age}year old")
